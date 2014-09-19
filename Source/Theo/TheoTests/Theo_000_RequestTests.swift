321b08973b2a34cc4af47d282893093843f52648
Cory D. Wiles <kwylez@gmail.com>
2014-09-19 15:55:35 -0400

    added a few more tests for accessing properties

--- Source/Theo/TheoTests/Theo_000_RequestTests.swift
+++ Source/Theo/TheoTests/Theo_000_RequestTests.swift
@@ -14,53 +14,130 @@ let TheoTimeoutInterval: NSTimeInterval = 10
 
 class Theo_000_RequestTestsTests: XCTestCase {
   
-  let baseURL: String = "https://graph-1095-neosl01-7665.sl-stories.graphstory.com"
-  
-  override func setUp() {
-    super.setUp()
-  }
-  
-  override func tearDown() {
-    super.tearDown()
-  }
-  
-  func test_000_successfullyFetchDBMeta() {
-    
-    let theo: Client = Client(baseURL: self.baseURL, user: "graph-1095", pass: "graph-1095")
-    let exp: XCTestExpectation = self.expectationWithDescription("test success \(self.baseURL)")
-    
-    theo.metaDescription({(meta, error) in
-      
-      println("meta \(meta) error \(error)")
-      
-      XCTAssert(meta != nil, "Meta can't be nil")
-      XCTAssert(error == nil, "Error must be nil \(error?.description)")
-      
-      exp.fulfill()
-    })
-    
-    self.waitForExpectationsWithTimeout(TheoTimeoutInterval){ error in
-      XCTAssertNil(error, "\(error)")
+    let baseURL: String = "https://graph-1095-neosl01-7665.sl-stories.graphstory.com"
+
+    override func setUp() {
+        super.setUp()
     }
-  }
-  
-  func test_001_failToFetchDBMeta() {
-    
-    let theo: Client = Client(baseURL: self.baseURL)
-    let exp: XCTestExpectation = self.expectationWithDescription("test fail \(self.baseURL)")
-    
-    theo.metaDescription({(meta, error) in
-      
-      println("meta \(meta) error \(error)")
-      
-      XCTAssert(meta == nil, "Meta must be nil")
-      XCTAssert(error != nil, "Error must be nil \(error?.description)")
-      
-      exp.fulfill()
-    })
-    
-    self.waitForExpectationsWithTimeout(TheoTimeoutInterval){ error in
-      XCTAssertNil(error, "\(error)")
+
+    override func tearDown() {
+        super.tearDown()
     }
-  }
+  
+    func test_000_successfullyFetchDBMeta() {
+
+        let theo: Client = Client(baseURL: self.baseURL, user: "graph-1095", pass: "sqS7FrpojNHjJZcJBWtu")
+        let exp = self.expectationWithDescription("test_000_successfullyFetchDBMeta")
+        theo.metaDescription({(meta, error) in
+          
+          println("meta in success \(meta) error \(error)")
+          
+          XCTAssert(meta != nil, "Meta can't be nil")
+          XCTAssert(error == nil, "Error must be nil \(error?.description)")
+          
+          exp.fulfill()
+        })
+
+        self.waitForExpectationsWithTimeout(10, handler: {error in
+          XCTAssertNil(error, "\(error)")
+        })
+    }
+  
+    func test_001_denyAnonymousFetchDBMeta() {
+
+        let theo: Client = Client(baseURL: self.baseURL, user: "", pass: "")
+        let exp = self.expectationWithDescription("test_001_denyAnonymousFetchDBMeta")
+
+        theo.metaDescription({(meta, error) in
+          
+            println("meta in deny \(meta) error \(error)")
+
+            XCTAssert(meta == nil, "Meta must be nil")
+            XCTAssert(error != nil, "Error must not be nil \(error?.description)")
+
+            exp.fulfill()
+        })
+
+        self.waitForExpectationsWithTimeout(10){ error in
+            XCTAssertNil(error, "\(error)")
+        }
+    }
+    
+    func test_002_successfullyFetchNode() {
+    
+        let theo: Client = Client(baseURL: self.baseURL, user: "graph-1095", pass: "sqS7FrpojNHjJZcJBWtu")
+        let exp = self.expectationWithDescription("test_002_successfullyFetchNode")
+        
+        theo.fetchNode(100, completionBlock: {(metaData, node, error) in
+
+            println("meta in success \(metaData) node \(node) error \(error)")
+            
+            XCTAssert(metaData != nil, "Meta data can't be nil")
+            XCTAssert(node != nil, "Node data can't be nil")
+            XCTAssert(error == nil, "Error must be nil \(error?.description)")
+            
+            exp.fulfill()
+        })
+        
+        self.waitForExpectationsWithTimeout(10, handler: {error in
+            XCTAssertNil(error, "\(error)")
+        })
+    }
+    
+    func test_003_successfullyAccessProperty() {
+    
+        let theo: Client = Client(baseURL: self.baseURL, user: "graph-1095", pass: "sqS7FrpojNHjJZcJBWtu")
+        let exp = self.expectationWithDescription("test_003_successfullyAccessProperty")
+        
+        theo.fetchNode(100, completionBlock: {(metaData, node, error) in
+            
+            println("meta in success \(metaData) node \(node) error \(error)")
+            
+            XCTAssert(metaData != nil, "Meta data can't be nil")
+            XCTAssert(node != nil, "Node data can't be nil")
+            XCTAssert(error == nil, "Error must be nil \(error?.description)")
+            
+            let nodeObject: Node = node!
+            let nodePropertyValue: AnyObject? = nodeObject.getProp("name")
+            
+            XCTAssert(nodePropertyValue != nil, "The nodeProperty can't be nil")
+            
+            println("nodePropertyValue \(nodePropertyValue!)")
+            
+            exp.fulfill()
+        })
+        
+        self.waitForExpectationsWithTimeout(10, handler: {error in
+            XCTAssertNil(error, "\(error)")
+        })
+    }
+    
+    func test_004_successfullyHandleNonExistantAccessProperty() {
+        
+        let theo: Client = Client(baseURL: self.baseURL, user: "graph-1095", pass: "sqS7FrpojNHjJZcJBWtu")
+        let exp = self.expectationWithDescription("test_003_successfullyAccessProperty")
+        let randomString: String = NSUUID.UUID().UUIDString
+        
+        theo.fetchNode(100, completionBlock: {(metaData, node, error) in
+            
+            println("meta in success \(metaData) node \(node) error \(error)")
+            
+            XCTAssert(metaData != nil, "Meta data can't be nil")
+            XCTAssert(node != nil, "Node data can't be nil")
+            XCTAssert(error == nil, "Error must be nil \(error?.description)")
+            
+            let nodeObject: Node = node!
+            let nodePropertyValue: AnyObject? = nodeObject.getProp(randomString)
+
+            XCTAssertNil(nodePropertyValue, "The nodeProperty must be nil")
+            
+            println("nodePropertyValue \(nodePropertyValue)")
+            
+            exp.fulfill()
+        })
+        
+        self.waitForExpectationsWithTimeout(10, handler: {error in
+            XCTAssertNil(error, "\(error)")
+        })
+    }
 }
