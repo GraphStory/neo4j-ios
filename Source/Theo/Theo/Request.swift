8f180c8298406b0bb4b2727a375954e8f0cd034a
Cory D. Wiles <kwylez@gmail.com>
2014-09-21 11:59:03 -0400

    formatting and added ability to make a post

--- Source/Theo/Theo/Request.swift
+++ Source/Theo/Theo/Request.swift
@@ -11,134 +11,194 @@ import Foundation
 typealias RequestSuccessBlock = (data: NSData?, response: NSURLResponse) -> Void
 typealias RequestErrorBlock   = (error: NSError, response: NSURLResponse) -> Void
 
-let GSTNetworkErrorDomain: String = "com.graphstory.network.error"
+let TheoNetworkErrorDomain: String = "com.theo.network.error"
 
 public struct AllowedHTTPMethods {
   
-  static var GET: String  = "GET"
-  static var PUT: String  = "PUT"
-  static var POST: String = "POST"
+    static var GET: String  = "GET"
+    static var PUT: String  = "PUT"
+    static var POST: String = "POST"
 }
 
 class Request: NSObject, NSURLSessionDelegate {
   
-  lazy var httpSession: Session = {
-    
-    Session.SessionParams.delegate = self
-    Session.SessionParams.queue = NSOperationQueue.mainQueue()
-    
-    return Session.sharedInstance;
-  }()
-  
-  lazy var sessionConfiguration: NSURLSessionConfiguration = {
-    return self.httpSession.configuration.sessionConfiguration
-  }()
-  
-  lazy var sessionHTTPAdditionalHeaders: [NSObject:AnyObject]? = {
-    return self.sessionConfiguration.HTTPAdditionalHeaders
-  }()
-  
-  let sessionURL: NSURL
-  
-  private var httpRequest: NSURLRequest
-  
-  required init(url: NSURL?, additionalHeaders:[String:String]?) {
-    
-    self.sessionURL  = url!
-    self.httpRequest = NSURLRequest(URL: self.sessionURL)
-    
-    super.init()
-    
-    if additionalHeaders != nil {
+    lazy var httpSession: Session = {
 
-      var newHeaders: [String:String] = [:]
+        Session.SessionParams.delegate = self
+        Session.SessionParams.queue = NSOperationQueue.mainQueue()
 
-      if let sessionConfigurationHeaders = self.sessionHTTPAdditionalHeaders as? [String:String] {
-      
-        for (origininalHeader, originalValue) in sessionConfigurationHeaders {
-          newHeaders[origininalHeader] = originalValue
-        }
-        
-        for (header, value) in additionalHeaders! {
-          newHeaders[header] = value
-        }
-      }
-      
-      self.sessionConfiguration.HTTPAdditionalHeaders = newHeaders as [NSObject:AnyObject]?
-      
-    } else {
-      
-      self.sessionURL = url!
+        return Session.sharedInstance;
+    }()
+  
+    lazy var sessionConfiguration: NSURLSessionConfiguration = {
+        return self.httpSession.configuration.sessionConfiguration
+    }()
+  
+    lazy var sessionHTTPAdditionalHeaders: [NSObject:AnyObject]? = {
+        return self.sessionConfiguration.HTTPAdditionalHeaders
+    }()
+  
+    let sessionURL: NSURL
+  
+    private var httpRequest: NSURLRequest
+    
+    deinit {
+//        self.httpSession.session.invalidateAndCancel()
     }
-  }
   
-  convenience override init() {
-    self.init(url: nil, additionalHeaders: nil)
-  }
-  
-  // MARK: Public Methods
-  
-  /// Method makes a basic HTTP get request
-  ///
-  /// :param: RequestSuccessBlock successBlock
-  /// :param: RequestErrorBlock errorBlock
-  /// :returns: Void
-  func getResource(successBlock: RequestSuccessBlock?, errorBlock: RequestErrorBlock?) -> Void {
+    required init(url: NSURL?, additionalHeaders:[String:String]?) {
     
-    var request: NSURLRequest = {
-      
-      let mutableRequest: NSMutableURLRequest = self.httpRequest.mutableCopy() as NSMutableURLRequest
-      
-      mutableRequest.HTTPMethod = AllowedHTTPMethods.GET
-      
-      return mutableRequest.copy() as NSURLRequest
-      }()
+        self.sessionURL  = url!
+        self.httpRequest = NSURLRequest(URL: self.sessionURL)
     
-    self.httpRequest = request
+        super.init()
     
-    let task : NSURLSessionDataTask = self.httpSession.session.dataTaskWithRequest(request, completionHandler: {(data: NSData!, response: NSURLResponse!, error: NSError!) -> Void in
+        if additionalHeaders != nil {
+
+            var newHeaders: [String:String] = [:]
+
+            if let sessionConfigurationHeaders = self.sessionHTTPAdditionalHeaders as? [String:String] {
       
-      var dataResp: NSData? = data
-      let httpResponse: NSHTTPURLResponse = response as NSHTTPURLResponse
-      let statusCode: Int = httpResponse.statusCode
-      let containsStatusCode:Bool = Request.acceptableStatusCodes().containsIndex(statusCode)
-      
-      if (!containsStatusCode) {
-        dataResp = nil
-      }
-      
-      if (successBlock != nil) {
-        successBlock!(data: dataResp, response: httpResponse)
-      }
-      
-      if (errorBlock != nil) {
+                for (origininalHeader, originalValue) in sessionConfigurationHeaders {
+                    newHeaders[origininalHeader] = originalValue
+                }
         
-        if (error != nil) {
-          errorBlock!(error: error, response: httpResponse)
+                for (header, value) in additionalHeaders! {
+                    newHeaders[header] = value
+                }
+            }
+      
+            self.sessionConfiguration.HTTPAdditionalHeaders = newHeaders as [NSObject:AnyObject]?
+      
+        } else {
+      
+            self.sessionURL = url!
         }
-        
-        if (!containsStatusCode) {
-          println("response \(response)")
-          let localizedErrorString: String = "There was an error processing the request"
-          let errorDictionary: [String:String] = ["NSLocalizedDescriptionKey" : localizedErrorString, "GSResponseCode" : "\(statusCode)"]
-          let requestResponseError: NSError = {
-            return NSError(domain: GSTNetworkErrorDomain, code: NSURLErrorUnknown, userInfo: errorDictionary)
+    }
+  
+    convenience override init() {
+        self.init(url: nil, additionalHeaders: nil)
+    }
+  
+    // MARK: Public Methods
+
+    /// Method makes a basic HTTP get request
+    ///
+    /// :param: RequestSuccessBlock successBlock
+    /// :param: RequestErrorBlock errorBlock
+    /// :returns: Void
+    func getResource(successBlock: RequestSuccessBlock?, errorBlock: RequestErrorBlock?) -> Void {
+    
+        var request: NSURLRequest = {
+      
+            let mutableRequest: NSMutableURLRequest = self.httpRequest.mutableCopy() as NSMutableURLRequest
+      
+            mutableRequest.HTTPMethod = AllowedHTTPMethods.GET
+      
+            return mutableRequest.copy() as NSURLRequest
             }()
+    
+        self.httpRequest = request
+    
+        let task : NSURLSessionDataTask = self.httpSession.session.dataTaskWithRequest(request, completionHandler: {(data: NSData!, response: NSURLResponse!, error: NSError!) -> Void in
+      
+            var dataResp: NSData? = data
+            let httpResponse: NSHTTPURLResponse = response as NSHTTPURLResponse
+            let statusCode: Int = httpResponse.statusCode
+            let containsStatusCode:Bool = Request.acceptableStatusCodes().containsIndex(statusCode)
+      
+            if (!containsStatusCode) {
+                dataResp = nil
+            }
+      
+            if (successBlock != nil) {
+                successBlock!(data: dataResp, response: httpResponse)
+            }
+      
+            if (errorBlock != nil) {
+        
+                if (error != nil) {
+                    errorBlock!(error: error, response: httpResponse)
+                }
+        
+                if (!containsStatusCode) {
+                    
+                    println("response \(response)")
+                    
+                    let localizedErrorString: String = "There was an error processing the request"
+                    let errorDictionary: [String:String] = ["NSLocalizedDescriptionKey" : localizedErrorString, "GSResponseCode" : "\(statusCode)"]
+                    let requestResponseError: NSError = {
+                        return NSError(domain: TheoNetworkErrorDomain, code: NSURLErrorUnknown, userInfo: errorDictionary)
+                    }()
           
-          errorBlock!(error: requestResponseError, response: httpResponse)
-        }
-      }
-    })
+                    errorBlock!(error: requestResponseError, response: httpResponse)
+                }
+            }
+        })
     
-    task.resume()
-  }
+        task.resume()
+    }
+    
+    func postResource(postData: AnyObject, successBlock: RequestSuccessBlock?, errorBlock: RequestErrorBlock?) -> Void {
+    
+        var request: NSURLRequest = {
+            
+            let mutableRequest: NSMutableURLRequest = self.httpRequest.mutableCopy() as NSMutableURLRequest
+            let transformedJSONData: NSData = NSJSONSerialization.dataWithJSONObject(postData, options: NSJSONWritingOptions(0), error: nil)!
+            
+            mutableRequest.HTTPMethod = AllowedHTTPMethods.POST
+            mutableRequest.HTTPBody   = transformedJSONData
+            
+            return mutableRequest.copy() as NSURLRequest
+        }()
+        
+        self.httpRequest = request
+
+        let task : NSURLSessionDataTask = self.httpSession.session.dataTaskWithRequest(request, completionHandler: {(data: NSData!, response: NSURLResponse!, error: NSError!) -> Void in
+            
+            var dataResp: NSData? = data
+            let httpResponse: NSHTTPURLResponse = response as NSHTTPURLResponse
+            let statusCode: Int = httpResponse.statusCode
+            let containsStatusCode:Bool = Request.acceptableStatusCodes().containsIndex(statusCode)
+            
+            if (!containsStatusCode) {
+                dataResp = nil
+            }
+            
+            if (successBlock != nil) {
+                successBlock!(data: dataResp, response: httpResponse)
+            }
+            
+            if (errorBlock != nil) {
+                
+                if (error != nil) {
+                    errorBlock!(error: error, response: httpResponse)
+                }
+                
+                if (!containsStatusCode) {
+                    
+                    println("response \(response)")
+                    
+                    let localizedErrorString: String = "There was an error processing the request"
+                    let errorDictionary: [String:String] = ["NSLocalizedDescriptionKey" : localizedErrorString, "TheoResponseCode" : "\(statusCode)"]
+                    let requestResponseError: NSError = {
+                        return NSError(domain: TheoNetworkErrorDomain, code: NSURLErrorUnknown, userInfo: errorDictionary)
+                    }()
+                    
+                    errorBlock!(error: requestResponseError, response: httpResponse)
+                }
+            }
+        })
+        
+        task.resume()
+    }
   
-  /// Defines and range of acceptable HTTP response codes. 200 thru 300 inclusive
-  /// :returns: NSIndexSet
-  class func acceptableStatusCodes() -> NSIndexSet {
+    /// Defines and range of acceptable HTTP response codes. 200 thru 300 inclusive
+    /// :returns: NSIndexSet
+    class func acceptableStatusCodes() -> NSIndexSet {
     
-    let nsRange = NSMakeRange(200, 100)
+        let nsRange = NSMakeRange(200, 100)
     
-    return NSIndexSet(indexesInRange: nsRange)
-  }
+        return NSIndexSet(indexesInRange: nsRange)
+    }
 }
