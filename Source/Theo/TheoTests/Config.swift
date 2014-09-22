e748a1df3f7f040a6042192a5892f0b4f8c96e72
Cory D. Wiles <kwylez@gmail.com>
2014-09-22 14:43:26 -0400

    Project meta

---
+++ Source/Theo/TheoTests/Config.swift
@@ -0,0 +1,28 @@
+//
+//  Config.swift
+//  Theo
+//
+//  Created by Cory D. Wiles on 9/21/14.
+//  Copyright (c) 2014 Theo. All rights reserved.
+//
+
+import Foundation
+
+
+struct Config {
+    let username: String
+    let password: String
+    let host: String
+    
+    init(pathToFile: String) {
+
+        let jsonData: NSData = NSData(contentsOfFile: pathToFile)
+        var jsonError: NSError?
+        let JSON: AnyObject? = NSJSONSerialization.JSONObjectWithData(jsonData, options: NSJSONReadingOptions(0), error: &jsonError) as AnyObject!
+        let jsonConfig: [String:String]! = JSON as [String:String]
+        
+        self.username = jsonConfig["username"]!
+        self.password = jsonConfig["password"]!
+        self.host     = jsonConfig["host"]!
+    }
+}
