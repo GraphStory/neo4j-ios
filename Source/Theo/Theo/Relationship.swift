d5ebe362558dc27ca1bd33d4db1260c18784ff7b
Cory D. Wiles <kwylez@gmail.com>
2014-09-23 10:53:50 -0700

    initial commit

---
+++ Source/Theo/Theo/Relationship.swift
@@ -0,0 +1,146 @@
+//
+//  Relationship.swift
+//  Theo
+//
+//  Created by Cory D. Wiles on 9/22/14.
+//  Copyright (c) 2014 Theo. All rights reserved.
+//
+
+import Foundation
+
+let RelationshipDataFromNodeKey = "fromNode"
+let RelationshipDataToNodeKey   = "toNode"
+let RelationshipDataTypeKey     = "type"
+
+struct RelationshipMeta: Printable {
+
+    let extensions: [String: AnyObject] = [String: AnyObject]()
+    let start: String                   = ""
+    let property: String                = ""
+    let relationship_self: String       = ""
+    let properties: String              = ""
+    let type: String                    = ""//TODO: add custom function so it will return RelationshipType
+    let end: String                     = ""
+    let data: [String: AnyObject]       = [String: AnyObject]()
+    
+    init(dictionaryResponse: Dictionary<String, AnyObject>!) {
+        
+        for (key: String, value: AnyObject) in dictionaryResponse {
+            
+            switch key {
+            case "extensions":
+                self.extensions = value as Dictionary
+            case "start":
+                self.start = value as String
+            case "property":
+                self.property = value as String
+            case "relationship_self":
+                self.relationship_self = value as String
+            case "properties":
+                self.properties = value as String
+            case "type":
+                self.type = value as String
+            case "end":
+                self.end = value as String
+            case "data":
+                self.data = value as Dictionary
+            default:
+                ""
+            }
+        }
+    }
+    
+    var description: String {
+        return "Extensions: \(self.extensions), start \(self.start), property \(self.property), self \(self.relationship_self), properties \(self.properties), type \(self.type), end \(self.end), data \(self.data))"
+    }
+}
+
+struct RelationshipType {
+
+    static var KNOWS: String   = "KNOWS"
+    static var know: String    = "know"
+    static var FRIENDS: String = "FRIENDS"
+    static var likes: String   = "likes"
+    static var has: String     = "has"
+    static var knows: String   = "knows"
+    static var LOVES: String   = "LOVES"
+}
+
+class Relationship {
+
+    private (set) var relationshipMeta: [String:AnyObject]       = [String:AnyObject]()
+    private (set) var relationshipCreateMeta: [String:AnyObject] = [String:AnyObject]()
+    private (set) var relationshipData: [String:AnyObject]       = [String:AnyObject]()
+
+    lazy var relationshipInfo: [String:AnyObject] = {
+        var info: [String:AnyObject] = [String:AnyObject]()
+        
+        info["to"]   = self.relationshipCreateMeta[RelationshipDataToNodeKey]
+        info["type"] = self.relationshipCreateMeta[RelationshipDataTypeKey]
+
+        if (!self.isDataEmpty()) {
+            info["data"] = self.relationshipData
+        }
+        
+        return info
+    }()
+
+    lazy var fromNode: String = {
+        
+        if let object: AnyObject = self.relationshipCreateMeta[RelationshipDataFromNodeKey] {
+            return object as String
+        }
+
+        return ""
+    }()
+    
+    lazy var toNode: String = {
+        
+        if let object: AnyObject = self.relationshipCreateMeta[RelationshipDataToNodeKey] {
+            return object as String
+        }
+        
+        return ""
+    }()
+    
+    lazy var relationshipType: String = {
+
+        if let object: AnyObject = self.relationshipCreateMeta[RelationshipDataTypeKey] {
+            return object as String
+        }
+        
+        return ""
+    }()
+    
+    init() {
+        self.relationshipCreateMeta = [String:AnyObject]()
+        self.relationshipData = [String: AnyObject]()
+    }
+    
+    func relate(fromNode: Node, toNode: Node, type: String) {
+    
+        self.relationshipCreateMeta[RelationshipDataFromNodeKey]  = fromNode.meta?.create_relationship
+        self.relationshipCreateMeta[RelationshipDataToNodeKey]    = toNode.meta?.nodeID()
+        self.relationshipCreateMeta[RelationshipDataTypeKey]      = type
+    }
+    
+    func getProp(propertyName: String) -> AnyObject? {
+        
+        if let object: AnyObject = self.relationshipData[propertyName] {
+            return object
+        }
+        
+        return nil
+    }
+    
+    func setProp(propertyName: String, propertyValue: String) -> Void {
+        
+        var objectValue: AnyObject = propertyValue as AnyObject
+        
+        self.relationshipData[propertyName] = objectValue
+    }
+    
+    func isDataEmpty() -> Bool {
+        return self.relationshipData.keys.isEmpty
+    }
+}
