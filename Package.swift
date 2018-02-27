822e16c73429de293235d316a27431d662d0375d
Niklas Saers <niklas@saers.com>
2018-02-27 16:47:18 +0100

    Update podspec and Package.swift

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "Theo",
     dependencies: [
-		.Package(url: "https://github.com/niklassaers/bolt-swift.git", majorVersion: 0),
+		.Package(url: "https://github.com/Neo4j-Swift/Bolt-swift.git", majorVersion: 1),
 		.Package(url: "https://github.com/antitypical/Result.git", majorVersion: 3)
 	],
     exclude: ["Source/Theo/TheoTests"]
